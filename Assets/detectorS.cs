1f78279e2f2427d9692089155c8c7e64a1230a84
dmtr <kabanov.dmitry.vladimirovich@gmail.com>
2019-11-15 14:51:58 +0300

    Initial commit

---
+++ Assets/detectorS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class detectorS : MonoBehaviour
+{
+    public int detected;
+    public int count;
+    public GameObject scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        scoreText.GetComponent<TextMesh>().text = (count - detected).ToString();
+        detected = 0;
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        detected++;
+    }
+}
